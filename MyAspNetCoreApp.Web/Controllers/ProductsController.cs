a25e248f6a836650ce4171b3c312542c446415b9
Ata <106282747+atakanberkyilmaz@users.noreply.github.com>
2023-11-17 22:15:33 +0300

    new update 17.11.2023 22:15

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -17,14 +17,14 @@ namespace MyAspNetCoreApp.Web.Controllers
            _productRepository = new ProductRepository();
            _context = context;
 
-            if (!_context.Products.Any())
-            {
-                _context.Products.Add(new Product() { Name = "Kalem 1", Price = 100, Stock = 100, Color= "Red" });
-                _context.Products.Add(new Product() { Name = "Kalem 2", Price = 200, Stock = 500, Color = "Red" });
-                _context.Products.Add(new Product() { Name = "Kalem 3", Price = 300, Stock = 300, Color = "Red" });
+            //if (!_context.Products.Any())
+            //{
+            //    _context.Products.Add(new Product() { Name = "Kalem 1", Price = 100, Stock = 100, Color= "Red" });
+            //    _context.Products.Add(new Product() { Name = "Kalem 2", Price = 200, Stock = 500, Color = "Red" });
+            //    _context.Products.Add(new Product() { Name = "Kalem 3", Price = 300, Stock = 300, Color = "Red" });
 
-                _context.SaveChanges();
-            }
+            //    _context.SaveChanges();
+            //}
             //if (!_productRepository.GetAll().Any()) { }
             //{
             //    _productRepository.Add(new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 200 });
@@ -48,9 +48,28 @@ namespace MyAspNetCoreApp.Web.Controllers
             //_productRepository.Remove(id);
             return RedirectToAction("Index");
         }
+        [HttpGet]
         public IActionResult Add() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveProduct() 
+        {
+            //1. Yöntem
+
+            var name = HttpContext.Request.Form["Name"].ToString();
+            var price = decimal.Parse(HttpContext.Request.Form["Price"].ToString());
+            var stock = int.Parse(HttpContext.Request.Form["Stock"].ToString());
+            var color = HttpContext.Request.Form["Color"].ToString();
+
+            Product newProduct = new Product() { Name = name, Price=price, Color = color, Stock = stock };
+
+            _context.Products.Add(newProduct); 
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Update(int id) 
